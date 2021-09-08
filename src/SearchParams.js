271dad4f6a33b8a766f015090065a8088a5c5658
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-09-08 21:39:00 +0530

    UseEffect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Pet } from "./Pet";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState();
-  const [breed, setBreed] = useState();
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    console.log(json);
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -56,6 +72,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
