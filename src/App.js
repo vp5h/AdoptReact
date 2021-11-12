93de7161ab6d23b8d5b431e9aeb385aeb914fbd5
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-11-13 01:07:41 +0530

    pre server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { StrictMode, useState} from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React, { StrictMode, useState } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
@@ -23,33 +22,28 @@ import ThemeContext from "./ThemeContext";
 
 const App = () => {
   const theme = useState("darkblue");
- 
+
   return (
-    <ThemeContext.Provider value={theme}>
-    <div>
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt Me!</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/">
-            <SearchParams />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-    </ThemeContext.Provider>
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <div>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </div>
+      </ThemeContext.Provider>
+    </StrictMode>
   );
 };
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+export default App;
---
+++ src/ClientApp.js
@@ -0,0 +1,12 @@
+import { hydrate } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+//other sttuff that only happens at browser
+
+hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
