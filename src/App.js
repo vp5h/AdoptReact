10b6f0db3dbcdc474f043bd81319126ddb3bd3c2
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-11-13 00:11:55 +0530

    lazy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { StrictMode, useState} from "react";
+import React, { StrictMode, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+// import Details from "./Details";
+// import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => {
+  import("./Details");
+});
+const SearchParams = lazy(() => {
+  import("./SearchParams");
+});
 // const App = () => {
 //   return React.createElement("div", {}, [
 //     React.createElement("h1", {}, "Adopt Me!"),
@@ -23,26 +30,26 @@ import ThemeContext from "./ThemeContext";
 
 const App = () => {
   const theme = useState("darkblue");
- 
+
   return (
     <ThemeContext.Provider value={theme}>
-    <div>
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt Me!</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/">
-            <SearchParams />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </ThemeContext.Provider>
   );
 };
